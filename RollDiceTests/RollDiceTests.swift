e88a08ffd26eaf7b6a938d56fe4cda7c84449761
luciano-dlr <delarubialuciano@gmail.com>
2024-09-19 15:55:47 -0300

    Initial Commit

---
+++ RollDiceTests/RollDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceTests.swift
+//  RollDiceTests
+//
+//  Created by Luciano de la Rubia on 19/09/2024.
+//
+
+import Testing
+@testable import RollDice
+
+struct RollDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ RollDiceUITests/RollDiceUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  RollDiceUITestsLaunchTests.swift
+//  RollDiceUITests
+//
+//  Created by Luciano de la Rubia on 19/09/2024.
+//
+
+import XCTest
+
+final class RollDiceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
